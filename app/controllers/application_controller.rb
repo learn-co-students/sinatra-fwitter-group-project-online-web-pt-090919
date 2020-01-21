c7145bd0b9b708f0519fa347cb1475e95540d6c3
Emil Andreasyan <emil.andreasyan@yahoo.com>
2020-01-21 05:33:37 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,5 +6,26 @@ class ApplicationController < Sinatra::Base
     set :public_folder, 'public'
     set :views, 'app/views'
   end
+  
+  get '/' do
+    erb :home
+  end
+  
+  def helpers do
+    def is_logged_in?
+      !!current_user
+    end
+    
+    def current_user
+      User.find_by(id: session[:user_id])
+    end
+    
+    def require_login
+      unless is_logged_in?
+      redirect "/"
+    end
+    end
+    
+  end
 
 end
--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,54 @@
 class TweetsController < ApplicationController
+  
+  before do
+    require_login
+  end
+
+get '/tweets' do
+  @tweets = Tweet.all
+  erb :'tweets/index'
+end
+
+get '/tweets/new' do
+  erb :'tweets/new'
+end
+
+post '/tweets' do
+  @tweet = current_user.tweets.build(params[:content])
+  if !params.empty?
+    @tweet.save
+    redirect "/tweets/#{@tweet.id}"
+  else
+    redirect "/tweets/new"
+  end
+end
+
+get '/tweets/:id' do
+  @tweet = Tweet.find(params[:id])
+  erb :'tweets/show'
+end
+
+get '/tweets/:id/edit' do
+  @tweet = Tweet.find(params[:id])
+  erb :'tweets/edit'
+end
+
+patch '/tweets/:id' do
+  @tweet = Tweet.find(params[:id])
+  if !params[:content].empty?
+    @tweet.update(params[:content])
+    redirect "/tweets/#{@tweet.id}"
+  else
+    redirect "/tweet/#{@tweet.id}/edit"
+  end 
+end
+
+delete "/tweet/:id" do
+  @tweet = Tweet.find(params[:id])
+  @tweet.destroy
+  redirect "/tweets"
+end
+
 
 
 end
